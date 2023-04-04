d3756936d23377d198ab8905c4f55275e9d4c521
Josefhdrdnghtz11 <117582021+Josefhdrdnghtz11@users.noreply.github.com>
2023-04-04 19:59:20 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 let data = [];
 
-function tampil(){
-    let tabel = document.getElementById('tabel');
-    tabel.innerHTML = '<tr><th>No</th><th>Jurusan</th><th>Action</th></tr>';
-    for (let i = 0; i < data.length; i++) {
-        let btnEdit = '<button class="btn-edit" onclick="edit('+i+')">edit</button>';
-        let btnHapus = '<button class="btn-hapus" onclick="hapus('+i+')">Hapus</button>';
-        j = i + 1;
-        tabel.innerHTML += `<tr><td>${j}</td><td>${data[i]}</td><td>${btnEdit}${btnHapus}</td></tr>`
-    }
+function tampil() {
+  let tabel = document.getElementById('tabel');
+  tabel.innerHTML = '<tr><th>No</th><th>Name</th><th>Jurusan</th><th>Action</th></tr>';
+  for (let i = 0; i < data.length; i++) {
+    let btnEdit = '<button class="btn-edit" onclick="edit(' + i + ')">edit</button>';
+    let btnHapus = '<button class="btn-hapus" onclick="hapus(' + i + ')">Hapus</button>';
+    let row = `<tr><td>${i + 1}</td><td>${data[i].name}</td><td>${data[i].jurusan}</td><td>${btnEdit}${btnHapus}</td></tr>`;
+    tabel.innerHTML += row;
+  }
 }
 
 function tambah() {
-    const input = document.querySelector('input[name="jurusan"]');
-    data.push(input.value);
-    tampil();
-    console.log(data);
+  const inputJurusan = document.querySelector('input[name="jurusan"]');
+  const inputName = document.querySelector('input[name="name"]');
+  const newItem = { name: inputName.value, jurusan: inputJurusan.value };
+  data.push(newItem);
+  tampil();
+  console.log(data);
 }
 
 function edit(index) {
-    let newValue = prompt('Masukkan nilai baru', data[index]);
-    if (newValue !== null) {
-        data[index] = newValue;
-        tampil();
-        console.log(data);
-    }
+  let newValue = prompt('Masukkan nilai baru', data[index].name);
+  if (newValue !== null) {
+    data[index].name = newValue;
+    tampil();
+    console.log(data);
+  }
 }
 
 function hapus(index) {
-    data.splice(index, 1);
-    tampil();
-    console.log(data);
+  data.splice(index, 1);
+  tampil();
+  console.log(data);
 }
 
 function update() {
-    let tabel = document.getElementById('tabel');
-    let rows = tabel.getElementsByTagName('tr');
-    for (let i = 1; i < rows.length; i++) {
-        let cells = rows[i].getElementsByTagName('td');
-        data[i-1] = cells[1].innerText;
-    }
-    console.log(data);
+  let tabel = document.getElementById('tabel');
+  let rows = tabel.getElementsByTagName('tr');
+  for (let i = 1; i < rows.length; i++) {
+    let cells = rows[i].getElementsByTagName('td');
+    data[i - 1].name = cells[1].innerText;
+    data[i - 1].jurusan = cells[2].innerText;
+  }
+  console.log(data);
 }
