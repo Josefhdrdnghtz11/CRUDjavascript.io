f4b06bc1011287e390de6f69dcf7188ddea51fa8
Josefhdrdnghtz11 <117582021+Josefhdrdnghtz11@users.noreply.github.com>
2023-04-07 11:21:12 +0700

    update

--- script2.js
+++ script2.js
@@ -18,59 +18,59 @@ function SubForm() {
         "Content-Type": "application/json"
       }
     })
-    .then(res => {
-      if (res.status === 201) {
-        // SUCCESS
-        alert("Data has been saved to spreadsheet!");
-        // Reset input values
-        document.querySelector('input[name="Nama"]').value = '';
-        document.querySelector('input[name="jurusan"]').value = '';
-        // Fetch and display updated data in table
-        fetchDataAndDisplayData();
-      } else {
-        // ERROR
-        alert("Failed to save data to spreadsheet.");
-      }
-    })
-    .catch(err => console.log(err));
+      .then(res => {
+        if (res.status === 201) {
+          // SUCCESS
+          alert("Data has been saved to spreadsheet!");
+          // Reset input values
+          document.querySelector('input[name="Nama"]').value = '';
+          document.querySelector('input[name="jurusan"]').value = '';
+          // Fetch and display updated data in table
+          fetchDataAndDisplayData();
+        } else {
+          // ERROR
+          alert("Failed to save data to spreadsheet.");
+        }
+      })
+      .catch(err => console.log(err));
   }
   
   // Function to fetch data from spreadsheet and display in table
   function fetchDataAndDisplayData() {
-    fetch("https://api.apispreadsheets.com/data/LQmdai9ervKESF0W/")
-    .then(res => res.json())
-    .then(data => {
-      if (data && Array.isArray(data)) {
-        displayData(data);
-      } else {
-        console.error('Failed to fetch data from spreadsheet.');
-      }
-    })
-    .catch(err => console.log(err));
-  }
+    fetch("https://api.apispreadsheets.com/data/LQmdai9ervKESF0W/?query=select%20*%20from%20LQmdai9ervKESF0W%20where%20No=1&count")
+      .then(res => {
+        if (res.status === 200) {
+          // SUCCESS
+          res.json().then(data => {
+            const yourData = data;
+            // Update your DOM manipulation logic with the fetched data
+            const tableBody = document.querySelector('.table-body'); // Update selector to match your tbody element's class or ID
+            if (!tableBody) {
+              console.error('Table body element not found in DOM');
+              return;
+            }
+            tableBody.innerHTML = '';
   
-  // Function to display data in table
-  function displayData(data) {
-    const tableBody = document.querySelector('.table-body'); // Update selector to match your tbody element's class or ID
-    if (!tableBody) {
-      console.error('Table body element not found in DOM');
-      return;
-    }
-    tableBody.innerHTML = '';
+            // Loop through data array and create table rows
+            yourData.forEach(item => {
+              const row = document.createElement('tr');
+              const namaCell = document.createElement('td');
+              const jurusanCell = document.createElement('td');
   
-    // Loop through data array and create table rows
-    data.forEach(item => {
-      const row = document.createElement('tr');
-      const namaCell = document.createElement('td');
-      const jurusanCell = document.createElement('td');
+              namaCell.textContent = item.Nama;
+              jurusanCell.textContent = item.Jurusan;
   
-      namaCell.textContent = item.Nama;
-      jurusanCell.textContent = item.Jurusan;
-  
-      row.appendChild(namaCell);
-      row.appendChild(jurusanCell);
-      tableBody.appendChild(row);
-    });
+              row.appendChild(namaCell);
+              row.appendChild(jurusanCell);
+              tableBody.appendChild(row);
+            });
+          }).catch(err => console.log(err));
+        } else {
+          // ERROR
+          console.error('Failed to fetch data from spreadsheet.');
+        }
+      })
+      .catch(err => console.log(err));
   }
   
   // Fetch and display data on page load
